97d7197510e8f1565719e7e5dde8a21c677178cb
thoriqmacto <thariq.hadad@gmail.com>
2025-09-05 17:24:11 +0300

    put back the JII symbol member

--- apps/api/config/csv.php
+++ apps/api/config/csv.php
@@ -4,11 +4,10 @@ use App\Services\IdxTicks;
 
 return [
     'seed_dir' => storage_path('app/historical'), // put your 30 CSVs here
-    'index_symbols' => ['ADRO','ANTM'],
-//    'index_symbols' => [
-//        'ADRO','AKRA','AMMN','ANTM','ASII','BRIS','BRMS','BRPT','CPIN','EXCL','ICBP','INCO',
-//        'INDF','INKP','ISAT','KLBF','MAPI','MBMA','MDKA','MEDC','PANI','PGAS','PGEO','PTBA',
-//        'PTRO','SMGR','TLKM','TPIA','UNTR','UNVR'],
+    'index_symbols' => [
+        'ADRO','AKRA','AMMN','ANTM','ASII','BRIS','BRMS','BRPT','CPIN','EXCL','ICBP','INCO',
+        'INDF','INKP','ISAT','KLBF','MAPI','MBMA','MDKA','MEDC','PANI','PGAS','PGEO','PTBA',
+        'PTRO','SMGR','TLKM','TPIA','UNTR','UNVR'],
     'default_lot_size' => 100,
     'tick_size' => [IdxTicks::class, 'tickFor'], // according to the IDX variable tick ladder.
     'chunk_size' => 200, // number of rows to insert per batch
