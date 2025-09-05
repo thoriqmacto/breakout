81a2eb5e4093868855d3bcd9391631f43a39b50a
thoriqmacto <thariq.hadad@gmail.com>
2025-09-05 17:08:44 +0300

    testing for little symbol first

--- apps/api/config/csv.php
+++ apps/api/config/csv.php
@@ -4,12 +4,12 @@ use App\Services\IdxTicks;
 
 return [
     'seed_dir' => storage_path('app/historical'), // put your 30 CSVs here
-    'index_symbols' => ['ADRO','AKRA','AMMN','ANTM','ASII','BRIS','BRMS','BRPT','CPIN','EXCL','ICBP','INCO',
-        'INDF','INKP','ISAT','KLBF','MAPI','MBMA','MDKA','MEDC','PANI','PGAS','PGEO','PTBA','PTRO','SMGR',
-        'TLKM','TPIA','UNTR','UNVR'],
+    'index_symbols' => ['ADRO','ANTM'],
+//    'index_symbols' => [
+//        'ADRO','AKRA','AMMN','ANTM','ASII','BRIS','BRMS','BRPT','CPIN','EXCL','ICBP','INCO',
+//        'INDF','INKP','ISAT','KLBF','MAPI','MBMA','MDKA','MEDC','PANI','PGAS','PGEO','PTBA',
+//        'PTRO','SMGR','TLKM','TPIA','UNTR','UNVR'],
     'default_lot_size' => 100,
-    // Callable that returns the appropriate tick size for a given price
-    // according to the IDX variable tick ladder.
-    'tick_size' => [IdxTicks::class, 'tickFor'],
+    'tick_size' => [IdxTicks::class, 'tickFor'], // according to the IDX variable tick ladder.
     'chunk_size' => 200, // number of rows to insert per batch
 ];
