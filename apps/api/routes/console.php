90b7185a9f1ac759492c5fcbac7ad5ddf005cc57
thoriqmacto <thariq.hadad@gmail.com>
2025-09-12 12:03:46 +0300

    add scheduler for eod retriever

--- apps/api/routes/console.php
+++ apps/api/routes/console.php
@@ -6,3 +6,8 @@ use Illuminate\Support\Facades\Artisan;
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+Schedule::command('asset:sync --eod')
+    ->dailyAt('13:00')
+    ->timezone('Asia/Qatar')
+    ->sendOutputTo(storage_path('logs/asset_sync.log'));
