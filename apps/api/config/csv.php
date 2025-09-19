1f569f9eb24bcbb91888a4b5365ba4a76fe8d85c
thoriqmacto <thariq.hadad@gmail.com>
2025-09-19 07:14:05 +0300

    update symbol as of 2025.09.18

--- apps/api/config/csv.php
+++ apps/api/config/csv.php
@@ -5,8 +5,8 @@ use App\Services\IdxTicks;
 return [
     'seed_dir' => database_path('seeders/data/historical'), // put your 30 CSVs here
     'index_symbols' => [
-        'ADRO','AKRA','AMMN','ANTM','ASII','BRIS','BRMS','BRPT','CPIN','DCII','EXCL','GEMS','ICBP','INCO',
-        'INDF','INDY','INKP','ISAT','KLBF','MAPI','MBMA','MDKA','MEDC','MIKA','MSKY','PANI','PGAS','PGEO',
+        'ACES','ADRO','AKRA','AMMN','ANTM','ASII','BRIS','BRMS','BRPT','CPIN','DCII','EXCL','GEMS','ICBP','INCO',
+        'INDF','INDY','INKP','ISAT','ITMG','KLBF','MAPI','MBMA','MDKA','MEDC','MIKA','MLPT', 'MSKY','PANI','PGAS','PGEO',
         'PTBA','PTRO','SMGR','SRTG','TLKM','TPIA','TSPC','UNTR','UNVR'],
     'default_lot_size' => 100,
     'tick_size' => [IdxTicks::class, 'tickFor'], // according to the IDX variable tick ladder.
