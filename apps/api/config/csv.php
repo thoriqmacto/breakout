ab454b4167d6095ae14be71451d674c333258a23
thoriqmacto <thariq.hadad@gmail.com>
2025-09-23 18:16:19 +0300

    add new symbol BUMI,CUAN,EMTK,SCMA,TEBE

--- apps/api/config/csv.php
+++ apps/api/config/csv.php
@@ -5,9 +5,9 @@ use App\Services\IdxTicks;
 return [
     'seed_dir' => database_path('seeders/data/historical'), // put your 30 CSVs here
     'index_symbols' => [
-        'ACES','ADRO','AKRA','AMMN','ANTM','ASII','BRIS','BRMS','BRPT','CPIN','DCII','EXCL','GEMS','ICBP','INCO',
+        'ACES','ADRO','AKRA','AMMN','ANTM','ASII','CUAN','BRIS','BRMS','BRPT','BUMI','CPIN','DCII','EMTK','EXCL','GEMS','ICBP','INCO',
         'INDF','INDY','INKP','ISAT','ITMG','KLBF','MAPI','MBMA','MDKA','MEDC','MIKA','MLPT', 'MSKY','PANI','PGAS','PGEO',
-        'PTBA','PTRO','SMGR','SRTG','TLKM','TPIA','TSPC','UNTR','UNVR'],
+        'PTBA','PTRO','SCMA','SMGR','SRTG','TEBE','TLKM','TPIA','TSPC','UNTR','UNVR'],
     'default_lot_size' => 100,
     'tick_size' => [IdxTicks::class, 'tickFor'], // according to the IDX variable tick ladder.
     'chunk_size' => 200, // number of rows to insert per batch
